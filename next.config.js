a066bd2b5a41256864846961f88b9dc03ca24ceb
Christian Nielsen <68676614+banner4422@users.noreply.github.com>
2021-08-17 00:01:35 +0200

    netlify plz

--- next.config.js
+++ next.config.js
@@ -2,5 +2,13 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.discordapp.com']
-  }
+  },
+  resolve: {
+    alias: {
+      'pg-native': 'noop2',
+      tedious: 'noop2',
+      sqlite3: 'noop2',
+      mysql2: 'noop2',
+    },
+  },
 }
